2b1afbe2b61da1b7104791df04c94d2408a180b1
mateusgms <mtsgomesp@gmail.com>
2021-01-05 19:05:09 -0300

    Feat:Making another svg into card

--- script.js
+++ script.js
@@ -1,34 +1,64 @@
 const equipamentos = [
-  { name: "link da Nigeria", id: 1, used: false },
-  { name: "Servidor da caverna", id: 2, used: false },
-  { name: "firewall endiabrado", id: 3, used: false }
+  {
+    name: "link da Nigeria",
+    id: 1,
+    used: false,
+    type: "link",
+    img: "./assets/db.svg"
+  },
+  {
+    name: "Servidor da caverna",
+    id: 2,
+    used: false,
+    type: "firewall",
+    img: "./assets/db.svg"
+  },
+  {
+    name: "firewall endiabrado",
+    id: 3,
+    used: false,
+    type: "link",
+    img: "./assets/db.svg"
+  }
 ];
 
 function getContext(contextName) {
   let context = document.getElementById(contextName);
   return context;
 }
-function createEquipamento() {}
-
+function createEquipamento() {
+  let select = getSelect();
+  let selectedEquip = select.selectedIndex;
+  let selectOptions = select.options;
+  let equip = selectOptions[selectedEquip].value;
+  console.log(equip);
+}
+//TODO
 function getEquipamento() {}
 
 function createSelect() {
   let context = getContext("sidebar");
   let selectEquip = document.createElement("SELECT");
   selectEquip.setAttribute("id", "selectEquip");
+
   context.appendChild(selectEquip);
 
   for (equip in equipamentos) {
     selectEquip.options[selectEquip.options.length] = new Option(
       equipamentos[equip].name,
-      name
+      equipamentos[equip].name
     );
   }
+  selectEquip.selectedIndex = "-1";
+  selectEquip.setAttribute("onchange", "createEquipamento()");
 }
-
+function getSelect() {
+  return selectEquip;
+}
 function createSvg() {
   let svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
   svg.setAttribute("id", 1);
+  svg.style.background = "red";
   return svg;
 }
 
@@ -38,16 +68,15 @@ function createCard() {
   const svgNS = svgContext.namespaceURI;
   let g = document.createElementNS(svgNS, "g");
   g.setAttribute("fill", "red");
-  let rect = document.createElementNS(svgNS, "rect");
+
   g.setAttribute("x", 0);
   g.setAttribute("y", 0);
-  rect.setAttribute("width", 500);
-  rect.setAttribute("height", 500);
+
   let btnCreateCard = document.getElementById("createCard");
   btnCreateCard.disabled = true;
 
   svgContext.appendChild(g);
-  g.append(rect);
+
   context.appendChild(svgContext);
   createSelect();
 }
